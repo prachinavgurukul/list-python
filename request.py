38e6f4472f9213ae43868e314a283dc632ef310b
prachinavgurukul <91555689+prachinavgurukul@users.noreply.github.com>
2022-05-07 12:25:49 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,24 @@
+# import requests
+# import json
+# url=requests.get("https://api.merakilearn.org/courses")
+# data=url.json()
+# with open("myfile.json","w") as f:
+#     json.dump(data,f,indent=4)
+# i=0
+# while i<len(data):
+#     print(str(i+1),":",data[i]["name"],data[i]["id"])
+#     i=i+1
+# a=int(input("enter the no:"))
+
+
+
+# import os.path
+# isExist=os.path.exists("myfile.json")
+# if isExist==True:
+#     print("exist")
+# else:
+#     print("not exist")
+
+
+
+
